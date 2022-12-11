3dbb9a7d78b1a90fcbf4d0d6e6005a3f2263c338
Nikita-PC\Nikita - Work <nikitawingategarbovsky@gmail.com>
2022-12-12 11:06:08 +1300

    implemented paralax effect between background and foreground

--- main.lua
+++ main.lua
@@ -11,7 +11,15 @@ VIRTUAL_HEIGHT = 288
 
 -- images we load into memory from files to later draw onto the screen
 local background = love.graphics.newImage('background.png')
+local backgroundScroll = 0
+
 local ground = love.graphics.newImage('ground.png')
+local groundScroll = 0
+
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+local BACKGROUND_LOOPING_POINT = 413
 
 function love.load()
     -- initialize our nearest-neighbor filter
@@ -38,14 +46,22 @@ function love.keypressed(key)
     end
 end
 
+function love.update(dt)
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt)
+        % BACKGROUND_LOOPING_POINT
+        
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt)
+        % VIRTUAL_WIDTH
+end
+
 function love.draw()
     push:start()
 
     -- draw the background starting at top left (0, 0)
-    love.graphics.draw(background, 0, 0)
+    love.graphics.draw(background, -backgroundScroll, 0)
 
     -- draw the ground on top of the background, toward the bottom of the screen
-    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
-    
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+
     push:finish()
 end
